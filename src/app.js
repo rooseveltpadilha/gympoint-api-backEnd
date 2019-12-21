4c90feca12c851b704dccbbb811fb5b13345287e
Roosevelt Padilha <roosevelttgp@gmail.com>
2019-12-21 03:55:06 -0300

    Adicionado o sucrase e server, feito o renomeamento de arquivos app.js e iniciado o sucrase e o nodemon

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-const app = require('./server');
-app.listen(3333);
+const express = require('express');
+const routes = require('./routes');
+
+class App { 
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
