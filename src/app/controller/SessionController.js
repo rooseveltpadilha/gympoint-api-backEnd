c71dce12fedcef2efeb97d233526c362bc8b3319
Roosevelt Padilha <roosevelttgp@gmail.com>
2019-12-21 10:15:13 -0300

    Adicionado o middleware para Auth e também fiz a adição do Yup para validação

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import authConf from '../../config/auth';
 import User from '../models/User';
 
@@ -6,6 +7,19 @@ class SessionController {
   // eslint-disable-next-line class-methods-use-this
   async store(req, res) {
     const { email, password } = req.body;
+
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(3),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
